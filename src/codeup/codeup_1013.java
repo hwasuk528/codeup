f4ece4866d78a7ba356698456230ca89aa93bc3f
hwaseok O <hwasuk528@naver.com>
2023-07-16 23:16:48 +0900

    codeup

---
+++ src/codeup/codeup_1013.java
@@ -0,0 +1,11 @@
+package codeup;
+import java.util.Scanner;
+
+public class codeup_1013 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		float i = sc.nextFloat();
+		System.out.println(String.format("%.6f", i));
+	}
+
+}
---
+++ src/codeup/codeup_1015.java
@@ -0,0 +1,12 @@
+package codeup;
+import java.util.Scanner;
+
+public class codeup_1015 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Float a = sc.nextFloat();
+
+		System.out.printf("%.2f", a);
+	}
+
+}
